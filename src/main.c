f5fdb05c474aefe1481c168cac9c9830e911bdcc
m4rcelrad <iamnotmarcel@gmail.com>
2026-02-21 18:28:03 +0100

    feat: add loan payment report generation and integrate CSV loading in `main.c`

--- src/main.c
+++ src/main.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataframe.h"
+#include "csv_reader.h"
+
+// #include "loan_simulation.h"
+// #include "report.h"
 
 int main(void) {
+
+    printf("Data Manager Module\n");
+
+    const char *target_file = "report.csv";
+    printf("Attempting to load: %s\n", target_file);
+
+    DataFrame *df = NULL;
+    DataframeErrorCode status = read_csv(target_file, true, ";", &df);
+
+    if (status == DATAFRAME_SUCCESS) {
+        printf("SUCCESS: File loaded perfectly.\n");
+
+        print_head_dataframe(df, 10);
+
+        free_dataframe(df);
+    } else {
+        printf("ERROR: Could not load file. Code: %d\n", status);
+    }
+
     return 0;
 }
