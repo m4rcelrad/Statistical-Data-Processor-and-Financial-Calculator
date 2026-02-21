758233e79891bd02ff59bbec0fbd7070093057c1
m4rcelrad <iamnotmarcel@gmail.com>
2026-02-21 18:07:35 +0100

    feat: move CSV reading logic to `csv_reader.c` with improved parsing and error handling

--- include/dataframe.h
+++ include/dataframe.h
@@ -1,28 +1,16 @@
 #ifndef STATISTICALDATAPROCESSOR_DATAFRAME_H
 #define STATISTICALDATAPROCESSOR_DATAFRAME_H
 
-#include <stdbool.h>
-
-typedef enum {
-    DATAFRAME_SUCCESS = 0,
-    DATAFRAME_ERR_FILE_NOT_FOUND,
-    DATAFRAME_ERR_EMPTY_FILE,
-    DATAFRAME_ERR_ALLOCATION_FAILED,
-    DATAFRAME_ERR_COLUMN_MISMATCH
-} DataframeErrorCode;
-
 typedef struct {
-  char **columns;
-  double **data;
-  int rows;
-  int cols;
+    char **columns;
+    double **data;
+    int rows;
+    int cols;
 } DataFrame;
 
-DataFrame* create_dataframe(int rows, int cols);
+DataFrame *create_dataframe(int rows, int cols);
 
 void free_dataframe(DataFrame *df);
-
-DataframeErrorCode read_csv(const char *path, bool has_header, const char *delim, DataFrame **out_df);
 
 void print_head_dataframe(const DataFrame *df, int limit);
 
