d6123bebb1b25a90ef48ebb724fc7bb828736337
m4rcelrad <iamnotmarcel@gmail.com>
2026-02-23 22:24:57 +0100

    refactor: enhance `aligned_calloc` with additional error checks for safety and robustness

--- src/memory_utils.c
+++ src/memory_utils.c
@@ -1,18 +1,33 @@
 #include "memory_utils.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #if defined(_MSC_VER)
 #include <malloc.h>
 #endif
 
 void* aligned_calloc(const size_t num, const size_t size, size_t alignment) {
-    size_t total_size = num * size;
-    if (total_size == 0 || alignment == 0) return NULL;
+    if (num == 0 || size == 0) return NULL;
 
+    if (alignment < sizeof(void*) || (alignment & (alignment - 1)) != 0) {
+        return NULL;
+    }
+
+    if (size > SIZE_MAX / num) {
+        return NULL;
+    }
+
+    size_t total_size = num * size;
     const size_t remainder = total_size % alignment;
     if (remainder != 0) {
-        total_size += alignment - remainder;
+        const size_t padding = alignment - remainder;
+
+        if (total_size > SIZE_MAX - padding) {
+            return NULL;
+        }
+
+        total_size += padding;
     }
 
     void *ptr = NULL;
