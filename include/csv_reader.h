57ab3ec7db1a489d763beb7ac88ceb14095b9ae4
m4rcelrad <iamnotmarcel@gmail.com>
2026-02-23 16:48:49 +0100

    feat: add support for heterogeneous data types in `DataFrame` and enhance CSV parsing

--- include/csv_reader.h
+++ include/csv_reader.h
@@ -4,14 +4,6 @@
 #include "dataframe.h"
 #include <stdbool.h>
 
-typedef enum {
-    DATAFRAME_SUCCESS = 0,
-    DATAFRAME_ERR_FILE_NOT_FOUND,
-    DATAFRAME_ERR_EMPTY_FILE,
-    DATAFRAME_ERR_ALLOCATION_FAILED,
-    DATAFRAME_ERR_COLUMN_MISMATCH
-} DataframeErrorCode;
-
 DataframeErrorCode read_csv(const char *path, bool has_header, const char *delim, DataFrame **out_df);
 
 #endif // STATISTICALDATAPROCESSOR_CSV_READER_H
--- include/dataframe.h
+++ include/dataframe.h
@@ -1,9 +1,28 @@
 #ifndef STATISTICALDATAPROCESSOR_DATAFRAME_H
 #define STATISTICALDATAPROCESSOR_DATAFRAME_H
 
+typedef enum {
+    DATAFRAME_SUCCESS = 0,
+    DATAFRAME_ERR_FILE_NOT_FOUND,
+    DATAFRAME_ERR_EMPTY_FILE,
+    DATAFRAME_ERR_ALLOCATION_FAILED,
+    DATAFRAME_ERR_COLUMN_MISMATCH
+} DataframeErrorCode;
+
+typedef enum {
+    TYPE_NUMERIC,
+    TYPE_STRING
+} DataType;
+
+typedef union {
+    double v_num;
+    char *v_str;
+} DataCell;
+
 typedef struct {
     char **columns;
-    double **data;
+    DataType *col_types;
+    DataCell **data;
     int rows;
     int cols;
 } DataFrame;
